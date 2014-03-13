3425cac5dac8b0b88b70944714e0676e2e6cf101
dgriffon <dgriffon@jahia.com>
2014-03-13 14:23:08 +0100

    QA-5720 : fix save i18n properties

--- core/src/main/java/org/jahia/modules/external/ExternalSessionImpl.java
+++ core/src/main/java/org/jahia/modules/external/ExternalSessionImpl.java
@@ -300,7 +300,7 @@ public class ExternalSessionImpl implements Session {
         for (Map.Entry<String, ExternalData> entry : changedData.entrySet()) {
             String path = entry.getKey();
             ExternalData externalData = entry.getValue();
-            if (path.startsWith(TRANSLATION_NODE_NAME_BASE,path.lastIndexOf("/"))) {
+            if (path.startsWith(TRANSLATION_NODE_NAME_BASE,path.lastIndexOf("/") + 1)) {
                 String lang = StringUtils.substringAfterLast(path, TRANSLATION_NODE_NAME_BASE);
                 String parentPath = StringUtils.substringBeforeLast(path, "/");
                 ExternalData parentData;
