9fd3314ad5087b739916e0cfc13ce420d87b3b33
david <dgriffon@jahia.com>
2015-06-17 09:54:19 +0200

    Revert "QA-7553 : use allowed chars instead of forbidden one for item name validation in definition"
    This reverts commit cada0615ab18cec57424f35083b869d65b93dc1c.

--- modules-provider/src/main/java/org/jahia/modules/external/modules/validator/ItemNameValidator.java
+++ modules-provider/src/main/java/org/jahia/modules/external/modules/validator/ItemNameValidator.java
@@ -94,7 +94,7 @@ public class ItemNameValidator implements JCRNodeValidator {
     }
 
 
-    @Pattern(regexp = "[A-Za-z]+[A-Za-z0-9:_]*")
+    @Pattern(regexp = "[^0-9*+\\-\\[\\]\\/\\|].[^+*\\-\\[\\]\\/\\|]*")
     public String getName() {
         return node.getName();
     }
