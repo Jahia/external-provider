c2e2fb2a0502e0683ef7234aec9f2d400282b27d
Thomas Draier <tdraier@jahia.com>
2018-05-15 11:46:37 +0200

    BACKLOG-7885 : Added method to check if event is coming from API

---
+++ core/src/main/java/org/jahia/modules/external/rest/ApiEventImpl.java
@@ -0,0 +1,130 @@
+/*
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2018 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.modules.external.rest;
+
+import org.apache.jackrabbit.util.ISO8601;
+import org.jahia.services.content.ApiEvent;
+
+import javax.jcr.observation.Event;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Implementation of event for Rest API
+ */
+public class ApiEventImpl implements ApiEvent {
+
+    private int type = Event.NODE_ADDED;
+    private String path;
+    private String userID;
+    private String identifier;
+    private Map info = new HashMap();
+    private String userData;
+    private long date = System.currentTimeMillis();
+
+    @Override
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    @Override
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    @Override
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    @Override
+    @XmlJavaTypeAdapter(EventInfoAdapter.class)
+    public Map getInfo() {
+        return info;
+    }
+
+    public void setInfo(Map info) {
+        this.info = info;
+    }
+
+    @Override
+    public String getUserData() {
+        return userData;
+    }
+
+    public void setUserData(String userData) {
+        this.userData = userData;
+    }
+
+    @Override
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = ISO8601.parse(date).getTimeInMillis();
+    }
+}
