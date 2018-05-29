138e92ef15ad8623f4b310e159035aa0a96c174a
Thomas Draier <tdraier@jahia.com>
2018-05-29 09:53:08 +0200

    BACKLOG-7885 : Added logging

--- core/src/main/java/org/jahia/modules/external/events/EventServiceImpl.java
+++ core/src/main/java/org/jahia/modules/external/events/EventServiceImpl.java
@@ -5,11 +5,15 @@ import org.jahia.modules.external.ExternalData;
 import org.jahia.modules.external.ExternalSessionImpl;
 import org.jahia.modules.external.events.model.ApiEventImpl;
 import org.jahia.services.content.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.jcr.RepositoryException;
 import java.util.stream.StreamSupport;
 
 public class EventServiceImpl implements EventService {
+
+    private static final Logger logger = LoggerFactory.getLogger(EventServiceImpl.class);
 
     public void sendEvents(Iterable<? extends ApiEvent> events, JCRStoreProvider provider) throws RepositoryException {
         doSendEvents(events, provider);
@@ -25,10 +29,13 @@ public class EventServiceImpl implements EventService {
     }
 
     public static void doSendEvents(Iterable<? extends ApiEvent> events, JCRStoreProvider provider) throws RepositoryException {
+        logger.info("Received API events for " + provider.getKey());
         JCRCallback<Object> callback = jcrSessionWrapper -> {
             for (ApiEvent apiEvent : events) {
+                logger.debug("Event {} for {}", apiEvent.getType(), apiEvent.getPath());
                 ExternalData data = (ExternalData) apiEvent.getInfo().get("externalData");
                 if (data != null) {
+                    logger.debug("External data included for {}",data.getPath());
                     ExternalSessionImpl externalSession = (ExternalSessionImpl) jcrSessionWrapper.getProviderSession(provider);
                     externalSession.registerNode(data);
                 }
@@ -37,8 +44,12 @@ public class EventServiceImpl implements EventService {
             return null;
         };
 
+        logger.debug("Processing API events for default");
         JCRObservationManager.doWorkspaceWriteCall(JCRSessionFactory.getInstance().getCurrentSystemSession(Constants.EDIT_WORKSPACE, null, null), JCRObservationManager.API, callback);
+        logger.debug("Processing API events for live");
         JCRObservationManager.doWorkspaceWriteCall(JCRSessionFactory.getInstance().getCurrentSystemSession(Constants.LIVE_WORKSPACE, null, null), JCRObservationManager.API, callback);
+
+        logger.info("API events processed");
     }
 
 }
