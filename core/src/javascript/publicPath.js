d1711a5234b61932d4e2da0f867535e2a8526d1b
Thomas Draier <tdraier@jahia.com>
2021-09-09 21:32:03 +0200

    BACKLOG-15989: Update dependencies, use webpack 5

--- core/src/javascript/publicPath.js
+++
@@ -1,2 +0,0 @@
-/* eslint-disable */
-__webpack_public_path__ = window.contextJsParameters.contextPath + '/modules/external-provider/javascript/apps/';
--- core/webpack.config.js
+++ core/webpack.config.js
@@ -3,78 +3,95 @@ const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-// Get manifest
-const normalizedPath = require('path').join(__dirname, './target/dependency');
-let manifest = '';
-
-require('fs').readdirSync(normalizedPath).forEach(function (file) {
-    manifest = './target/dependency/' + file;
-    console.log('Server Settings module uses manifest: ' + manifest);
-});
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const shared = require("./webpack.shared")
 
 module.exports = (env, argv) => {
     let config = {
         entry: {
-            main: [path.resolve(__dirname, 'src/javascript/publicPath'), path.resolve(__dirname, 'src/javascript/index.js')]
+            main: [path.resolve(__dirname, 'src/javascript/index.js')]
         },
         output: {
-            jsonpFunction: 'jahiaServerSettingsJsonp',
             path: path.resolve(__dirname, 'src/main/resources/javascript/apps/'),
-            filename: 'jahia.bundle.js',
-            chunkFilename: '[name].jahia.[chunkhash:6].js'
+            filename: 'edp.bundle.js',
+            chunkFilename: '[name].edp.[chunkhash:6].js'
         },
         resolve: {
             mainFields: ['module', 'main'],
             extensions: ['.mjs', '.js', '.jsx', 'json']
         },
-        optimization: {
-            splitChunks: {
-                maxSize: 400000
-            }
-        },
         module: {
             rules: [
                 {
-                    test: /\.jsx?$/,
-                    include: [path.join(__dirname, 'src')],
-                    loader: 'babel-loader',
-                    query: {
-                        presets: [
-                            ['@babel/preset-env', {modules: false, targets: {safari: '7', ie: '10'}}],
-                            '@babel/preset-react'
-                        ],
-                        plugins: [
-                            '@babel/plugin-syntax-dynamic-import'
-                        ]
-                    }
+                    test: /\.m?js$/,
+                    type: 'javascript/auto'
                 },
                 {
-                    test: /\.s[ac]ss$/i,
+                    test: /\.jsx?$/,
+                    include: [path.join(__dirname, 'src')],
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', {modules: false, targets: {safari: '7', ie: '10'}}],
+                                '@babel/preset-react'
+                            ],
+                            plugins: [
+                                '@babel/plugin-syntax-dynamic-import'
+                            ]
+                        }
+                    }
+                },
+                  {
+                    test: /\.css$/,
+                    sideEffects: true,
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.scss$/i,
+                    sideEffects: true,
                     use: [
                         'style-loader',
+                        // Translates CSS into CommonJS
                         {
-                            loader:'css-loader',
+                            loader: 'css-loader',
                             options: {
-                                modules: true
+                                modules: {
+                                    mode: 'local'
+                                }
                             }
                         },
+                        // Compiles Sass to CSS
                         'sass-loader'
                     ]
+                },
+                {
+                    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }]
                 }
             ]
         },
         plugins: [
-            new webpack.DllReferencePlugin({
-                manifest: require(manifest)
+            new ModuleFederationPlugin({
+                name: "edp",
+                library: { type: "assign", name: "appShell.remotes.edp" },
+                filename: "remoteEntry.js",
+                exposes: {
+                    './init': './src/javascript/init',
+                },
+                remotes: {
+                    '@jahia/app-shell': 'appShellRemote',
+                },
+                shared
             }),
             new CleanWebpackPlugin({verbose: false}),
-            new webpack.HashedModuleIdsPlugin({
-                hashFunction: 'sha256',
-                hashDigest: 'hex',
-                hashDigestLength: 20
-            }),
-            new CopyWebpackPlugin([{from: './package.json', to: ''}])
+            new CopyWebpackPlugin({patterns: [{from: './package.json', to: ''}]}),
         ],
         mode: argv.mode
     };
