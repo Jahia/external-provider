32c97b4d753da5e352f07cdaff7e6c50aa7d10d5
Thomas Draier <tdraier@jahia.com>
2023-01-19 11:26:53 +0100

    Improved tests

--- tests/cypress/e2e/vfs-jcontent.cy.ts
+++ tests/cypress/e2e/vfs-jcontent.cy.ts
@@ -1,4 +1,5 @@
-import {JContent} from '@jahia/content-editor-cypress/dist/page-object/jcontent';
+import {JContent as JContentCE} from '@jahia/content-editor-cypress/dist/page-object/jcontent';
+import {JContent} from '@jahia/jcontent-cypress/dist/page-object/jcontent';
 
 describe('VFS local mount tests', () => {
     beforeEach(function () {
@@ -11,9 +12,18 @@ describe('VFS local mount tests', () => {
         cy.executeGroovy('cleanup.groovy');
     });
 
+    it('should see the file in jcontent', function () {
+        cy.login();
+        const jc = JContent.visit('digitall', 'en', 'media/files');
+        jc.switchToListMode();
+        jc.getTable().get().contains('mount-test').dblclick();
+        jc.getTable().get().contains('image').dblclick();
+        jc.getTable().get().contains('tomcat.gif');
+    });
+
     it('should be able to create a ref', function () {
         cy.login();
-        const jc = JContent.visit('digitall', 'en', 'content-folders/contents');
+        const jc = JContentCE.visit('digitall', 'en', 'content-folders/contents');
         const ce = jc.createContent('File reference');
         const picker = ce.getPickerField('jnt:fileReference_j:node', false).open();
         picker.switchViewMode('List');
--- tests/cypress/e2e/vfs.cy.ts
+++ tests/cypress/e2e/vfs.cy.ts
@@ -1,65 +1,17 @@
-import gql from 'graphql-tag';
-
-describe('VFS global mount tests', () => {
-    beforeEach(function () {
-        cy.executeGroovy('cleanup.groovy');
-        cy.executeGroovy('createDir.groovy');
-        cy.apollo({queryFile: 'mount.graphql'});
-    });
-
-    afterEach(function () {
-        cy.executeGroovy('cleanup.groovy');
-    });
-
+const commonTests = (path => {
     it('should be able to get files in live', function () {
-        cy.request('/files/live/mounts/mount-test-mountPoint/images/tomcat.gif');
+        cy.request(`/files/live${path}/images/tomcat.gif`);
     });
 
     it('should be able to get files in edit when logged', function () {
         cy.login();
-        cy.request('/files/default/mounts/mount-test-mountPoint/images/tomcat.gif');
-    });
-
-    it('should be displayed in admin', function () {
-        cy.login();
-        cy.visit('http://localhost:8080/cms/adminframe/default/en/settings.manageMountPoints.html?redirect=false');
-        cy.contains('mount-test-mountPoint');
+        cy.request(`/files/default${path}/images/tomcat.gif`);
     });
 
     it('should be able to create folder with root', function () {
-        cy.apollo({
-            mutation: gql`mutation {
-                jcr {
-                    mutateNode(pathOrId: "/mounts/mount-test-mountPoint") {
-                        addChild(name: "toto", primaryNodeType: "jnt:folder") {
-                            uuid
-                        }
-                    }
-                }
-            }`
-        });
+        cy.apollo({mutationFile: 'createFolder.graphql', variables: {path}});
         cy.executeGroovy('checkFile.groovy', {'#path#': '/tmp/mount-test/toto'}).should(r => {
             expect(r).to.eq('true');
-        });
-    });
-
-    it('should not be able to create folder with an editor', function () {
-        cy.apolloClient({username: 'mathias', password: 'password'}).apollo({
-            mutation: gql`mutation {
-                jcr {
-                    mutateNode(pathOrId: "/mounts/mount-test-mountPoint") {
-                        addChild(name: "toto", primaryNodeType: "jnt:folder") {
-                            uuid
-                        }
-                    }
-                }
-            }`,
-            errorPolicy: 'all'
-        }).should(({errors}) => {
-            expect('javax.jcr.AccessDeniedException: /').to.be.oneOf(errors.map(e => e.message));
-        });
-        cy.executeGroovy('checkFile.groovy', {'#path#': '/tmp/mount-test/toto'}).should(r => {
-            expect(r).to.eq('false');
         });
     });
 
@@ -67,65 +19,92 @@ describe('VFS global mount tests', () => {
         cy.executeGroovy('checkFile.groovy', {'#path#': '/tmp/mount-test/index.html'}).should(r => {
             expect(r).to.eq('true');
         });
-        cy.apollo({
-            mutation: gql`mutation {
-                jcr {
-                    mutateNode(pathOrId: "/mounts/mount-test-mountPoint/index.html") {
-                        delete
-                    }
-                }
-            }`
-        });
+        cy.apollo({mutationFile: 'deleteNode.graphql', variables: {path: `${path}/index.html`}});
         cy.executeGroovy('checkFile.groovy', {'#path#': '/tmp/mount-test/index.html'}).should(r => {
             expect(r).to.eq('false');
         });
     });
 
     it('should be able to copy file from jcr to vfs', function () {
-        cy.apollo({
-            mutation: gql`mutation {
-                jcr {
-                    copyNode(pathOrId:"/sites/digitall/files/images/people/portrait-bill-taylor.jpg", destParentPathOrId:"/mounts/mount-test-mountPoint") {
-                        uuid
-                    }
-                }
-            }`
-        });
+        cy.apollo({mutationFile: 'copyNode.graphql', variables: {path}});
         cy.executeGroovy('checkFile.groovy', {'#path#': '/tmp/mount-test/portrait-bill-taylor.jpg'}).should(r => {
             expect(r).to.eq('true');
         });
     });
 
     it('should be able to add tags mixin', function () {
-        cy.apollo({
-            mutation: gql`mutation m {
-                jcr {
-                    mutateNode(pathOrId:"/mounts/mount-test-mountPoint/images/tomcat.gif") {
-                        addMixins(mixins:["jmix:tagged"])
-                        mutateProperty(name:"j:tagList") {
-                            setValues(values:["tag1", "tag2"])
-                        }
-                    }
-                }
-            }`
-        });
-        cy.apollo({
-            query: gql`query q {
-                jcr {
-                    nodeByPath(path:"/mounts/mount-test-mountPoint/images/tomcat.gif"){
-                        mixinTypes {
-                            name
-                        }
-                        property(name:"j:tagList") {
-                            values
-                        }
-                    }
-                }
-            }`
-        }).should(({data}) => {
+        cy.apollo({mutationFile: 'addTags.graphql', variables: {path: `${path}/images/tomcat.gif`}});
+        cy.apollo({queryFile: 'getTags.graphql', variables: {path: `${path}/images/tomcat.gif`}}).should(({data}) => {
             expect('jmix:tagged').to.be.oneOf(data.jcr.nodeByPath.mixinTypes.map(m => m.name));
             expect('tag1').to.be.oneOf(data.jcr.nodeByPath.property.values);
             expect('tag2').to.be.oneOf(data.jcr.nodeByPath.property.values);
+        });
+    });
+});
+
+describe('VFS mount operations tests', () => {
+    beforeEach(function () {
+        cy.executeGroovy('cleanup.groovy');
+        cy.executeGroovy('createDir.groovy');
+    });
+
+    afterEach(function () {
+        cy.apollo({mutationFile: 'deleteReference.graphql', errorPolicy: 'all'});
+        cy.executeGroovy('cleanup.groovy');
+    });
+
+    describe('on a global mount point', () => {
+        beforeEach(function () {
+            cy.apollo({queryFile: 'mount.graphql'});
+        });
+
+        commonTests('/mounts/mount-test-mountPoint');
+
+        it('should not be able to create folder with an editor', function () {
+            cy.apolloClient({username: 'mathias', password: 'password'}).apollo({
+                mutationFile: 'createFolder.graphql',
+                variables: {path: '/mounts/mount-test-mountPoint'},
+                errorPolicy: 'all'
+            }).should(({errors}) => {
+                expect('javax.jcr.AccessDeniedException: /').to.be.oneOf(errors.map(e => e.message));
+            });
+            cy.executeGroovy('checkFile.groovy', {'#path#': '/tmp/mount-test/toto'}).should(r => {
+                expect(r).to.eq('false');
+            });
+        });
+
+        it('should be available at new path after switching from global to local', function () {
+            cy.apollo({mutationFile: 'moveToLocal.graphql'});
+            cy.request('/files/live/sites/digitall/files/mount-test/images/tomcat.gif');
+        });
+
+        it('should keep references after switching from global to local', function () {
+            cy.apollo({mutationFile: 'createReference.graphql'});
+            cy.apollo({queryFile: 'getReference.graphql'}).should(({data}) => {
+                expect(data.jcr.nodeByPath.property.refNode.path).eq('/mounts/mount-test-mountPoint/images/tomcat.gif');
+            });
+            cy.apollo({mutationFile: 'moveToLocal.graphql'});
+            cy.apollo({queryFile: 'getReference.graphql'}).should(({data}) => {
+                expect(data.jcr.nodeByPath.property.refNode.path).eq('/sites/digitall/files/mount-test/images/tomcat.gif');
+            });
+        });
+    });
+
+    describe('on a local mount point', () => {
+        beforeEach(function () {
+            cy.apollo({queryFile: 'mountLocal.graphql'});
+        });
+
+        commonTests('/sites/digitall/files/mount-test');
+
+        it('should be able to create folder with an editor', function () {
+            cy.apolloClient({username: 'mathias', password: 'password'}).apollo({
+                mutationFile: 'createFolder.graphql',
+                variables: {path: '/sites/digitall/files/mount-test'}
+            });
+            cy.executeGroovy('checkFile.groovy', {'#path#': '/tmp/mount-test/toto'}).should(r => {
+                expect(r).to.eq('true');
+            });
         });
     });
 });
