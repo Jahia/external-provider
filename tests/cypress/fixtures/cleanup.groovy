cc4a3aa07828f0d366c1b1c2f275699ca7c3a4fe
Thomas Draier <tdraier@jahia.com>
2023-02-06 16:31:15 +0100

    TECH-1115: Added tests with acls (#97)

--- tests/cypress/fixtures/cleanup.groovy
+++ tests/cypress/fixtures/cleanup.groovy
@@ -1,8 +1,28 @@
+import javax.jcr.Session
+import javax.jcr.query.Query
+
 org.jahia.services.content.JCRTemplate.getInstance().doExecuteWithSystemSession(session -> {
     def ni = session.getNode('/mounts').getRealNode().getNodes()
     while (ni.hasNext()) {
         ni.next().remove();
     }
+
     session.save()
+
+    def jrSession  = session.getProviderSession(session.getNode('/').getProvider());
+    def toDel = [];
+    def ni2 = jrSession.getWorkspace().getQueryManager().createQuery('SELECT * FROM [jnt:externalProviderExtension]', Query.JCR_SQL2).execute().getNodes()
+    while (ni2.hasNext()) {
+        toDel.push(ni2.next());
+    }
+
+    for (node in toDel) {
+        try {
+            node.remove()
+        } catch (Exception e) {
+        }
+    }
+
+    jrSession.save()
 });
 new File("/tmp/mount-test").deleteDir()
