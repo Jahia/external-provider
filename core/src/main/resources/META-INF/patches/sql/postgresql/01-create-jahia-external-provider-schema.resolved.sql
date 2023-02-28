d89c427103dfa3409e0bf752a14f1f210db3ab67
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2023-02-28 15:28:43 -0500

    Revert "BACKLOG-17331: Replace text fields by oid (#98)" (#103)
    This reverts commit 7517042da57ff7feb944a06d88b0298aae70f4a8.

--- core/src/main/resources/META-INF/patches/sql/postgresql/01-create-jahia-external-provider-schema.resolved.sql
+++ core/src/main/resources/META-INF/patches/sql/postgresql/01-create-jahia-external-provider-schema.resolved.sql
@@ -1,7 +1,7 @@
 
     create table jahia_external_mapping (
         internalUuid varchar(36) not null,
-        externalId oid not null,
+        externalId text not null,
         externalIdHash int4,
         providerKey varchar(255) not null,
         primary key (internalUuid)
@@ -18,5 +18,3 @@
     create index jahia_external_provider_id_ix1 on jahia_external_provider_id (providerKey);
 
     create sequence jahia_external_provider_id_seq;
-
-    CREATE TRIGGER t_oid_jahia_external_mapping_externalid BEFORE DELETE OR UPDATE ON jahia_external_mapping FOR EACH ROW EXECUTE FUNCTION lo_manage('externalid');
--- core/src/main/resources/META-INF/patches/sql/postgresql/02-updateTextToOid.resolved.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS lo;
-
-ALTER TABLE jahia_external_mapping ADD COLUMN new_column oid;
-UPDATE jahia_external_mapping SET new_column = cast(externalid as oid);
-ALTER TABLE jahia_external_mapping DROP COLUMN externalid;
-ALTER TABLE jahia_external_mapping RENAME COLUMN new_column TO externalid;
-
-DROP TRIGGER IF EXISTS t_oid_jahia_external_mapping_externalid ON jahia_external_mapping;
-CREATE TRIGGER t_oid_jahia_external_mapping_externalid BEFORE DELETE OR UPDATE ON jahia_external_mapping FOR EACH ROW EXECUTE FUNCTION lo_manage('externalid');
