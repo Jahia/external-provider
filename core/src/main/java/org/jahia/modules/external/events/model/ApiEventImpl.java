1c701e39597d01955254edd1d9415636210476f3
Thomas Draier <tdraier@jahia.com>
2023-09-29 09:58:34 +0200

    use javax.validation

--- core/src/main/java/org/jahia/modules/external/events/model/ApiEventImpl.java
+++ core/src/main/java/org/jahia/modules/external/events/model/ApiEventImpl.java
@@ -17,13 +17,13 @@ package org.jahia.modules.external.events.model;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.jackrabbit.util.ISO8601;
-//import org.hibernate.validator.constraints.NotEmpty;
 import org.jahia.modules.external.ExternalData;
 import org.jahia.modules.external.events.validation.ValidExternalData;
 import org.jahia.modules.external.events.validation.ValidISO8601;
 import org.jahia.services.content.ApiEvent;
 
 import javax.jcr.observation.Event;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.util.HashMap;
@@ -56,7 +56,7 @@ public class ApiEventImpl implements ApiEvent {
     @Pattern(regexp = "NODE_ADDED|NODE_REMOVED|PROPERTY_ADDED|PROPERTY_REMOVED|PROPERTY_CHANGED|NODE_MOVED")
     private String type = "NODE_ADDED";
 
-//    @NotEmpty
+    @NotEmpty
     private String path;
     private String userID = StringUtils.EMPTY;
     private String identifier;
