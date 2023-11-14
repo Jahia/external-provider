ddc99b75cb8115413161f01908765539f49358a8
Geofrey Flores <gflores@jahia.com>
2023-11-14 16:15:48 -0500

    Add graphql API endpoints and osgi service

---
+++ core/src/main/java/org/jahia/modules/external/graphql/AdminQueryExtension.java
@@ -0,0 +1,42 @@
+/*
+ * ==========================================================================================
+ * =                            JAHIA'S ENTERPRISE DISTRIBUTION                             =
+ * ==========================================================================================
+ *
+ *                                  http://www.jahia.com
+ *
+ * JAHIA'S ENTERPRISE DISTRIBUTIONS LICENSING - IMPORTANT INFORMATION
+ * ==========================================================================================
+ *
+ *     Copyright (C) 2002-2023 Jahia Solutions Group. All rights reserved.
+ *
+ *     This file is part of a Jahia's Enterprise Distribution.
+ *
+ *     Jahia's Enterprise Distributions must be used in accordance with the terms
+ *     contained in the Jahia Solutions Group Terms &amp; Conditions as well as
+ *     the Jahia Sustainable Enterprise License (JSEL).
+ *
+ *     For questions regarding licensing, support, production usage...
+ *     please contact our team at sales@jahia.com or go to http://www.jahia.com/license.
+ *
+ * ==========================================================================================
+ */
+package org.jahia.modules.external.graphql;
+
+import graphql.annotations.annotationTypes.GraphQLDescription;
+import graphql.annotations.annotationTypes.GraphQLField;
+import graphql.annotations.annotationTypes.GraphQLName;
+import graphql.annotations.annotationTypes.GraphQLTypeExtension;
+import org.jahia.modules.graphql.provider.dxm.admin.GqlAdminQuery;
+
+@GraphQLTypeExtension(GqlAdminQuery.class)
+@GraphQLDescription("Query extensions for mount point")
+public class AdminQueryExtension {
+
+    @GraphQLField
+    @GraphQLName("mountPoint")
+    @GraphQLDescription("Mount point mutation extension API")
+    public static GqlMountPointQuery mountPoint() {
+        return new GqlMountPointQuery();
+    }
+}
---
+++ core/src/main/java/org/jahia/modules/external/graphql/GqlMountPointExtensionsProvider.java
@@ -0,0 +1,42 @@
+/*
+ * ==========================================================================================
+ * =                            JAHIA'S ENTERPRISE DISTRIBUTION                             =
+ * ==========================================================================================
+ *
+ *                                  http://www.jahia.com
+ *
+ * JAHIA'S ENTERPRISE DISTRIBUTIONS LICENSING - IMPORTANT INFORMATION
+ * ==========================================================================================
+ *
+ *     Copyright (C) 2002-2023 Jahia Solutions Group. All rights reserved.
+ *
+ *     This file is part of a Jahia's Enterprise Distribution.
+ *
+ *     Jahia's Enterprise Distributions must be used in accordance with the terms
+ *     contained in the Jahia Solutions Group Terms &amp; Conditions as well as
+ *     the Jahia Sustainable Enterprise License (JSEL).
+ *
+ *     For questions regarding licensing, support, production usage...
+ *     please contact our team at sales@jahia.com or go to http://www.jahia.com/license.
+ *
+ * ==========================================================================================
+ */
+package org.jahia.modules.external.graphql;
+
+import org.jahia.modules.graphql.provider.dxm.DXGraphQLExtensionsProvider;
+import org.osgi.service.component.annotations.Component;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Main GraphQL extension provider for external provider module
+ */
+@Component(immediate = true, service=DXGraphQLExtensionsProvider.class)
+public class GqlMountPointExtensionsProvider implements DXGraphQLExtensionsProvider {
+
+    @Override
+    public Collection<Class<?>> getExtensions() {
+        return Arrays.<Class<?>>asList(AdminMutationExtension.class, AdminQueryExtension.class);
+    }
+}
