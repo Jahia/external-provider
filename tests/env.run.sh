46e2c07e9d8f2e607925afe3188080e66c478468
Geofrey Flores <gflores@jahia.com>
2023-11-14 20:19:45 -0500

    force install order

--- tests/env.run.sh
+++ tests/env.run.sh
@@ -55,11 +55,21 @@ if [[ -d artifacts/ && $MANIFEST == *"build"* ]]; then
   echo "$(date +'%d %B %Y - %k:%M') [MODULE_INSTALL] == Will start submitting files"
   for file in $(ls -1 *-SNAPSHOT.jar | sort -n)
   do
-    echo "$(date +'%d %B %Y - %k:%M') [MODULE_INSTALL] == Submitting module from: $file =="
-    curl -u root:${SUPER_USER_PASSWORD} -X POST ${JAHIA_URL}/modules/api/provisioning --form script='[{"installAndStartBundle":"'"$file"'", "forceUpdate":true}]' --form file=@$file
-    echo
-    echo "$(date +'%d %B %Y - %k:%M') [MODULE_INSTALL] == Module submitted =="
+    if [[ $file != external-provider-ui* ]]; then
+      echo "$(date +'%d %B %Y - %k:%M') [MODULE_INSTALL] == Submitting module from: $file =="
+      curl -u root:${SUPER_USER_PASSWORD} -X POST ${JAHIA_URL}/modules/api/provisioning --form script='[{"installAndStartBundle":"'"$file"'", "forceUpdate":true}]' --form file=@$file
+      echo
+      echo "$(date +'%d %B %Y - %k:%M') [MODULE_INSTALL] == Module submitted =="
+    fi
   done
+  
+  # submit external-provider-ui last
+  file=$(ls -1 external-provider-ui*-SNAPSHOT.jar)
+  echo "$(date +'%d %B %Y - %k:%M') [MODULE_INSTALL] == Submitting module from: $file =="
+  curl -u root:${SUPER_USER_PASSWORD} -X POST ${JAHIA_URL}/modules/api/provisioning --form script='[{"installAndStartBundle":"'"$file"'", "forceUpdate":true}]' --form file=@$file
+  echo
+  echo "$(date +'%d %B %Y - %k:%M') [MODULE_INSTALL] == Module submitted =="
+
   cd ..
 fi
 
