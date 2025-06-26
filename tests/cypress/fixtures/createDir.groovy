88e291004201f9488ab24f6da09f6f106498170d
David Griffon <dgriffon@jahia.com>
2025-06-26 10:45:51 +0200

    test: fix vfs failing test by not relying on external resources. (#196)

--- tests/cypress/fixtures/createDir.groovy
+++ tests/cypress/fixtures/createDir.groovy
@@ -1,3 +1,6 @@
 new File("/tmp/mount-test").mkdir()
-org.apache.commons.io.FileUtils.copyDirectory(new File('/usr/local/tomcat/webapps.dist/docs'), new File('/tmp/mount-test'));
+new File("/tmp/mount-test/images").mkdir()
 
+// Create dummy files used for testing
+new File("/tmp/mount-test/images/tomcat.gif").createNewFile()
+new File("/tmp/mount-test/index.html").createNewFile()
